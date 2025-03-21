7739266921002172454660b1d17dbafb9f7f716c
PabloTerroba <uo289073@uniovi.es>
2025-03-21 18:34:06 +0100

    Add files via upload

---
+++ MathService/MathService/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathService;
+
+public class Math : IMath
+{
+    public bool Prime(int n)
+    {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
